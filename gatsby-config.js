f1aa0e928b898104a237661026b76fef06c08c26
Julian V <jvalle0926@gmail.com>
2019-11-24 16:28:47 -0800

    changed meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `Atole Media`,
+    author: `Julian Valle`,
+    description: `A multimedia site serving delicously artful content to warm the heart`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `uncannvalle`,
     },
   },
   plugins: [
