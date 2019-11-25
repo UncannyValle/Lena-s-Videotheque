58a443d32ee6e8c3ee0ce3e8da388029c6e4d5b8
Julian V <jvalle0926@gmail.com>
2019-11-24 23:09:51 -0800

    more bug tests

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,26 +15,28 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./final-logo.jpg" alt="atole media logo" />
-        <h1 className="cool">
-          Take a warm sip of creativity{" "}
-
-            
-        </h1>
+        <img
+          style={{ margin: 0 }}
+          src= {"../images/final-logo.jpg"}
+          alt="atole media logo"
+        />
+        <h1 className="cool">Take a warm sip of creativity </h1>
         <p>
           Atole Media is a creative studio specializing in storytelling through
           web comics, web games, animations and concept art.
         </p>
         <p>
-          We provide a means to escape reality, to step away from the day to day 
-          and into the fantastical. The word Atole is the Mexican word of a 
+          We provide a means to escape reality, to step away from the day to day
+          and into the fantastical. The word Atole is the Mexican word of a
           traditional hot drink made of maize, and calls forth the Mexican art
           of storytelling, of sitting around the table, nursing a cup of this
-          magical beverage. Only then is one ready to be part of story. 
+          magical beverage. Only then is one ready to be part of story.
         </p>
         <p> All you have to do is sit back and drink the Atole!</p>
         <Link to="/blog/">
-          <Button marginTop="35px" color='red'>Go to Blog</Button>
+          <Button marginTop="35px" color="red">
+            Go to Blog
+          </Button>
         </Link>
       </Layout>
     )
