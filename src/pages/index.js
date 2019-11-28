ebf164c814961cafd4c24a349edf6d2d009d1597
Julian V <jvalle0926@gmail.com>
2019-11-27 16:50:36 -0800

    css changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
 import logo from "../images/final-logo.jpg"
-
+// import "../styles/Righteous-Regular.ttf"
 class IndexPage extends React.Component {
   render() {
     const siteTitle = "Atole Media"
@@ -15,28 +15,25 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img
-          style={{ margin: 0 }}
-          src= {logo}
-          alt="atole media logo"
-        />
-        <h1
-          style={{color:"#f97976"}}
-        >Take  warm sip of creativity </h1>
-        <p style={{color:"purple"}}>
+        <img style={{ margin: 0 }} src={logo} alt="atole media logo" />
+        <h1 style={{ color: "#f97976" }}>Take warm sip of creativity </h1>
+        <p style={{ color: "purple" }}>
           Atole Media is a creative studio specializing in storytelling through
           web comics, web games, animations and concept art.
         </p>
-        <p style={{color:"purple"}}>
+        <p style={{ color: "purple" }}>
           We provide a means to escape reality, to step away from the day to day
           and into the fantastical. The word Atole is the Mexican word of a
           traditional hot drink made of maize, and calls forth the Mexican art
           of storytelling, of sitting around the table, nursing a cup of this
           magical beverage. Only then is one ready to be part of story.
         </p>
-        <p style={{color:"purple"}}> All you have to do is sit back and drink the Atole!</p>
+        <p style={{ color: "purple" }}>
+          {" "}
+          All you have to do is sit back and drink the Atole!
+        </p>
         <Link to="/blog/">
-          <Button margin="50px 0">
+          <Button marginTop={"50px"} marginBottom={"50px"}>
             Go to Blog
           </Button>
         </Link>
